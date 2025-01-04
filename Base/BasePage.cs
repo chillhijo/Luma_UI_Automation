58d61dffacc961ff862b92c51a1caf8ae20589de
chillhijo <pralicadusko93@gmail.com>
2025-01-04 20:20:56 +0100

    BasePage updated with some methods

--- Base/BasePage.cs
+++ Base/BasePage.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Microsoft.Playwright;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace Luma_UI_Automation.Base
 {
-    internal class BasePAge
+    public class BasePage(IPage page)
     {
+        protected IPage Page = page;
+
+        public static async Task ClickOnElementAsync(ILocator element)
+        {
+            await element.ClickAsync();
+        }
+
+        public static async Task IsVisibleAsync(ILocator element)
+        {
+            var isVisible = await element.IsVisibleAsync();
+            Assert.That(isVisible, Is.True);
+        }
+
+        public static async Task EnterTextAsync(ILocator element, string text)
+        {
+            await element.FillAsync(text);
+        }
     }
 }
