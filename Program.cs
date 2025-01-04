198a1a73f233d81aa3d69c3a258045b0c7a634cd
chillhijo <pralicadusko93@gmail.com>
2025-01-04 19:49:24 +0100

    Add .gitignore and initial files

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Playwright;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var playwright = await Playwright.CreateAsync();
+        var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false });
+        var page = await browser.NewPageAsync();
+        await page.GotoAsync("https://example.com");
+        Console.WriteLine(await page.TitleAsync());
+        await browser.CloseAsync();
+    }
+}
